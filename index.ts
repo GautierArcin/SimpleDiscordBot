5003039514b6267892fd94b483be714702ad6636
Gautier Arcin <gauta82@gmail.com>
2021-06-14 11:00:09 +0000

    cleanup

--- index.ts
+++ index.ts
@@ -6,10 +6,7 @@ const result = dotenv.config();
 if (result.error) {
   throw result.error;
 }
-// console.log(result.parsed)
 
-// Getting Discord.js
-// const Discord = require('discord.js');
 import * as Discord from "discord.js";
 
 // Creating a discord client
@@ -17,10 +14,11 @@ const client = new Discord.Client();
 
 // We need to run it just one time and when the client is ready
 // Because then it will get undefined if the client isn't ready
-client.once("ready", (): boolean => {
+client.once("ready", (): void => {
   if (client == null) throw "Client undefined";
   const name = client?.user?.tag;
   console.log(`Online as ${name}`);
+
   if (
     process.env.GUILD == undefined ||
     process.env.CHANNEL == null ||
@@ -30,15 +28,15 @@ client.once("ready", (): boolean => {
 
   const guild = client.guilds.cache.get(process.env.GUILD);
   if (guild == null) throw "Guild undefined";
+
   const channel = guild.channels.cache.get(process.env.CHANNEL);
   const Message = process.env.MESSAGE;
-
   if (channel == null || channel.type !== "text") throw "Channel undefined";
+
   (channel as Discord.TextChannel)
     .send(Message)
     .then(() => client.destroy())
     .catch((e) => console.error("send error : ", e));
-  return true;
 });
 
 if (process.env.TOKEN == null)
